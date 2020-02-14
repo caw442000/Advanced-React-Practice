600361718776da54743611324e805d60c3dc1956
cedric-winbush <cawinbushjr@gmail.com>
2020-02-13 18:18:29 -0600

    create class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import axios from 'axios';
+
 
 import './App.css';
 
-function App() {
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    };
+  }
+
   
+  render(){ 
     return (
     <div className="App">
       <h1>Happy building <span role="img" aria-label="Smiling emoji">😃</span></h1>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
